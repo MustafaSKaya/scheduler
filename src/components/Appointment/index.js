06cb9625a3d66f9f1cff2b0845bdfda57d04a0e6
MustafaSKaya <mustafakya88@hotmail.com>
2022-01-20 21:06:06 -0500

    I forgot push any changes I made so its a lot of changes

---
+++ src/components/Appointment/index.js
@@ -0,0 +1,16 @@
+import React, {Fragment} from 'react';
+import 'components/Appointment/styles.scss';
+import Header from './Header';
+import Show from './Show';
+import Empty from './Empty';
+
+export default function Appointment(props) {
+
+  return(
+    <Fragment>
+      <Header time={props.time}></Header>
+      {props.interview ? <Show student={props.interview.student} interviewer={props.interview.interviewer} ></Show> : 
+      props.id === "last" ? null : <Empty id={props.id}></Empty>}
+    </Fragment>
+  )
+};
---
+++ src/components/InterviewerListItem.js
@@ -0,0 +1,21 @@
+import React, { useState } from "react";
+import classNames from "classnames";
+import "components/InterviewerListItem.scss"
+
+export default function InterviewerListItem(props) {
+
+    const { interviewer, setInterviewer} = useState()
+    
+    let listClass = classNames('interviewers__item', { "interviewers__item--selected": props.selected });
+
+    return (
+        <li className={listClass} onClick={props.setInterviewer}>
+            <img
+                className="interviewers__item-image"
+                src={props.avatar}
+                alt={props.name}
+            />
+            {props.selected ? props.name : null}
+        </li>
+    );
+};
