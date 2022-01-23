a52ca32cb7a6a5d9619016517122a8f1b4c3beda
MustafaSKaya <mustafakya88@hotmail.com>
2022-01-23 18:29:39 -0500

    useVisualMode and its test is added

---
+++ src/hooks/useVisualMode.js
@@ -0,0 +1,22 @@
+import React, { useState } from "react"
+
+export default function useVisualMode(initialMode) {
+  const [mode, setMode] = useState(initialMode);
+  const [history, setHistory] = useState([initialMode]);
+
+  function transition(newMode, replace = false) {
+    if (!replace) {
+      setHistory([...history, newMode]);
+    }
+    setMode(newMode);
+  }
+
+  function back() {
+    if (history.length > 1) {
+      history.pop();
+      setMode(history[history.length-1])
+    }
+  }
+  
+  return {mode, transition, back};
+}
